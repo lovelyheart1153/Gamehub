4bf880d548221ee0c668842896894b2ed61af6e3
unknown <genesisserver1153@gmail.com>
2024-06-23 22:20:57 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import { Button } from "@chakra-ui/react";
-import ColorModeSwitch from "./components/ColorModeSwitch";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-   return <ColorModeSwitch />;
+   return (
+      <Grid
+         templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`,
+         }}
+      >
+         <GridItem area="nav" bg="coral">
+            Nav
+         </GridItem>
+         <Show above="lg">
+            <GridItem area="aside" bg="blue">
+               Aside
+            </GridItem>
+         </Show>
+         <GridItem area="main" bg="yellow">
+            Main
+         </GridItem>
+      </Grid>
+   );
 }
 
 export default App;
