5da2a6aad278f90a29825a4c70aacef73aca9440
unknown <genesisserver1153@gmail.com>
2024-06-25 09:07:29 +0100

    Implement a query object

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,18 +3,14 @@ import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import { Genre } from "../hooks/useGenres";
+import { GameQuery } from "../App";
 
 interface Props {
-   selectedGenre: Genre | null;
-   selectedPlatform: Platform | null;
+   gameQuery: GameQuery;
 }
 
-const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
-   const {
-      data: games,
-      error,
-      isLoading,
-   } = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({ gameQuery }: Props) => {
+   const { data: games, error, isLoading } = useGames(gameQuery);
    const skeletons = [...Array(11).keys()];
 
    return (
